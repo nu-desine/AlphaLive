c34f4439b32bd36f2868b5fc2e82edaacf89f04d
Liam Lacey <liamlacey@hotmail.com>
2014-06-09 15:35:43 +0100

    Added translate() calls to the new strings in the last commit.

--- Source/Functionality Classes/AlphaLiveEngine.cpp
+++ Source/Functionality Classes/AlphaLiveEngine.cpp
@@ -1195,10 +1195,10 @@ void AlphaLiveEngine::actionListenerCallback (const String& message)
                         //sleep to make it more clear that a new alert has popped up...
                         #if JUCE_WINDOWS
                         sleep(500); //should this actually be Sleep() which need a windows library defined?
-						instructionsString = "To allow the AlphaSphere to be updated, please unplug the device, hold down the reset button (next to the USB port), and plug the device back in. If the AlphaSphere is now flashing red, release the reset button and the device will successfully enter the updater mode. If Windows is now installing new drivers for the device please wait for the drivers to be installed, and then press OK to continue.";
+						instructionsString = translate("To allow the AlphaSphere to be updated, please unplug the device, hold down the reset button (next to the USB port), and plug the device back in. If the AlphaSphere is now flashing red, release the reset button and the device will successfully enter the updater mode. If Windows is now installing new drivers for the device please wait for the drivers to be installed, and then press OK to continue.");
                         #else
                         usleep(500*1000);
-						instructionsString = "To allow the AlphaSphere to be updated, please unplug the device, hold down the reset button (next to the USB port), and plug the device back in. If the AlphaSphere is now flashing red, release the reset button and the device will successfully enter the updater mode. If Press OK to continue.";
+						instructionsString = translate("To allow the AlphaSphere to be updated, please unplug the device, hold down the reset button (next to the USB port), and plug the device back in. If the AlphaSphere is now flashing red, release the reset button and the device will successfully enter the updater mode. If Press OK to continue.");
                         #endif
                         
                         bool shouldContinue = AlertWindow::showOkCancelBox (AlertWindow::NoIcon, 

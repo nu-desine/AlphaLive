0a7ccbfaa71d4ad7fa2d58c0e8f78d1f2c1f1ee3
Liam Lacey <liamlacey@hotmail.com>
2014-07-29 16:31:19 +0100

    Upped the version number to 1.3.1 for Windows.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -37,8 +37,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "AlphaLive";
-    const char* const  versionString  = "1.3.0";
-    const int          versionNumber  = 0x10300;
+    const char* const  versionString  = "1.3.1";
+    const int          versionNumber  = 0x10301;
 }
 
 #endif   // __APPHEADERFILE_F22PMI__

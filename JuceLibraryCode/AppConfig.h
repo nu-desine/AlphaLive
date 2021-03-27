52adba915483db6b02bea8c72addf65877a7013e
Liam Lacey <liamlacey@hotmail.com>
2021-03-27 08:18:21 +0000

    Re-enabled JUCE_ASIO and JUCE_JACK  which got overriden in JUCE 6 upgrade.

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -87,7 +87,7 @@
 #endif
 
 #ifndef    JUCE_ASIO
- //#define JUCE_ASIO 0
+ #define   JUCE_ASIO 1
 #endif
 
 #ifndef    JUCE_WASAPI
@@ -103,7 +103,7 @@
 #endif
 
 #ifndef    JUCE_JACK
- //#define JUCE_JACK 0
+ #define   JUCE_JACK 1
 #endif
 
 #ifndef    JUCE_BELA
